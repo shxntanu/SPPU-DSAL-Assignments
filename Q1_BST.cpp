148367e1b915e53a2915d4e9f9c0b072c81bff99
Shantanu Wable <97496261+shxntanu@users.noreply.github.com>
2023-02-17 15:33:37 +0530

    Update Q1_BST.cpp

--- Q1_BST.cpp
+++ Q1_BST.cpp
@@ -1,9 +1,11 @@
 /*
 Beginning with an empty binary search tree, Construct binary search tree by inserting
 the values in the order given. After constructing a binary tree -
-i. Insert new node, ii. Find number of nodes in longest path from root, iii. Minimum data
-value found in the tree, iv. Change a tree so that the roles of the left and right pointers
-are swapped at every node, v. Search a value
+i.      Insert new node, 
+ii.     Find number of nodes in longest path from root, 
+iii.    Minimum data value found in the tree, 
+iv.     Change a tree so that the roles of the left and right pointers are swapped at every node, 
+v.      Search a value.
 */
 
 #include <iostream>
@@ -29,12 +31,15 @@ class BST
     Node *root, *temp;
 
     void insert_node(int d, Node *n);   //done
-    void display(Node *t);              //done
-    int find_longest_path();            
+    void display(Node *t);              //done           
     int min_value();                    //done
     int max_value();                    //done
     bool search(int d, Node *n);        //done
-    void swap_nodes();
+    void swap_all_nodes(Node *n);       //Error
+    void swap(Node *n1, Node *n2);
+    int height(Node* node);             //done
+    int dia(Node *node);                //done
+    void del();
 };
 
 int main()
@@ -42,19 +47,26 @@ int main()
     BST tree;
     int choice;
     int flag = 1;
-    int d; bool result;
+    int d,path; bool result;
     cout<<"\nEnter root node: ";
     int root;
     cin>>root;
     tree.root->data = root;
     while(flag==1)
     {
-        cout<<"~~~~~ BST MENU ~~~~~"<<endl;
-        cout<<"1. Insert Node"<<endl;
-        cout<<"2. Search for a Node"<<endl;
-        cout<<"3. Display BST"<<endl;
-        cout<<"4. Display Smallest Value"<<endl;
-        cout<<"5. Display largest value"<<endl;
+        cout<<"======================================="<<endl;
+        cout<<"||                                   ||"<<endl;
+        cout<<"||       ~~~~~ BST MENU ~~~~~        ||"<<endl;
+        cout<<"||                                   ||"<<endl;
+        cout<<"||       1. Insert Node              ||"<<endl;
+        cout<<"||       2. Search for a Node        ||"<<endl;
+        cout<<"||       3. Display BST              ||"<<endl;
+        cout<<"||       4. Display Smallest Value   ||"<<endl;
+        cout<<"||       5. Display largest value    ||"<<endl;
+        cout<<"||       6. Swap all nodes           ||"<<endl;
+        cout<<"||       7. Longest Path             ||"<<endl;
+        cout<<"||                                   ||"<<endl;
+        cout<<"======================================="<<endl;
         cout<<"\nEnter your choice: ";
         cin>>choice;
 
@@ -82,6 +94,14 @@ int main()
                     break;
                     
             case 5: cout<<"Largest value is: "<<tree.max_value()<<endl;
+                    break;
+            
+            case 6: tree.swap_all_nodes(tree.root);
+                    cout<<"All Nodes swapped!"<<endl;
+                    break;
+
+            case 7: path=tree.dia(tree.root)-1;
+                    cout<<"Longest Path: "<<path<<endl;
                     break;
 
             default:cout<<"Wrong Input"<<endl;
@@ -179,4 +199,76 @@ void BST::display(Node *t)
     display(t->left);
     cout<<t->data<<" ";
     display(t->right);
+}
+
+void BST::swap(Node *n1, Node *n2)
+{
+    Node *temp;
+    temp = n1;
+    n1 = n2;
+    n2 = temp;
+    delete temp;
+}
+
+void BST::swap_all_nodes(Node *n)
+{
+    if(n == NULL)
+        return;
+
+    else if(n->left == NULL)
+    {
+        if(n->right == NULL)
+            return;
+        
+        else
+        {    
+            swap(n->left, n->right);
+            swap_all_nodes(n->right);
+        }
+
+    }
+
+    else if(n->right == NULL)
+    {
+        if(n->left == NULL)
+            return;
+        
+        else
+        {
+            swap(n->right, n->left);
+            swap_all_nodes(n->left);
+        }
+    }
+
+    else
+    {
+        swap(n->left, n->right);
+        swap_all_nodes(n->right);
+        swap_all_nodes(n->left);
+    }
+}
+
+int BST::height(Node* node)
+{
+    if(node == NULL) return 0;
+
+    int h1 = height(node->left);
+    int h2 = height(node->right);
+
+    return max(h1,h2)+1;
+}
+
+int BST::dia(Node* node)
+{
+    if(node == NULL) return 0;
+
+    int l = height(node->left);
+    int r = height(node->right);
+
+    int op1 = dia(node->left);
+    int op2 = dia(node->right);
+    int op3 = l+r+1;
+
+    return max(op1,max(op2,op3));
+
 }
