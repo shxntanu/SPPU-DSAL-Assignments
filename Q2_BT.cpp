de5d18da660d37c32d9dbb86dec28abde085beea
Shantanu Wable <97496261+shxntanu@users.noreply.github.com>
2023-02-26 13:12:02 +0530

    Update Q2_BT.cpp

--- Q2_BT.cpp
+++ Q2_BT.cpp
@@ -21,160 +21,100 @@ using namespace std;
 
 class Node
 {
+public:
     Node *left;
     int data;
     Node *right;
-    public:
-    Node(int d=0)
+    
+    Node(int d)
     {
-        int d=0;
-        left = right = NULL;
+        left = NULL;
+        data = d;
+        right = NULL;
     }
-
-    friend class BT;
-};
-
-class BT
-{
-    Node *root = new Node();
-
-    public:
-    BT()
+    
+    Node()
     {
-        int d;
-        cout<<"Enter value of root node of tree";
-        cin>>d;
-        root->data = d;
-    }
-
-    void insert(Node *n, int d);
-    void rinorder(Node *node);
-    void rpreorder(Node *node);
-    void rpostorder(Node *node);
-    void iinorder(Node *node);
-    void ipreorder(Node *node);
-    void ipostorder(Node *node);
-    void swap(Node *node);
-    int height(Node* node);
-    int dia(Node* node);
-
-    BT(BT *t)
-    {
-        this->root = t->root;
+        left = NULL;
+        data = 0;
+        right = NULL;
     }
 };
 
-void BT::insert(Node *n, int d)
+Node* buildTree(Node *root)
 {
-    Node *newnode = new Node(d);
-    if(n==NULL)
-        root = newnode;
-
-    else
-    {
-        queue<Node*> q;
-        q.push(n);
+    cout<<"Enter data for node (-1 indicates no data): ";
+    int d;
+    cin>>d;
+    root = new Node(d);
     
-        while (!q.empty()) {
-            Node* temp = q.front();
-            q.pop();
+    if(d == -1)
+        return NULL;
     
-            if (temp->left != NULL)
-                q.push(temp->left);
-            else {
-                temp->left = newnode;
-            }
-    
-            if (temp->right != NULL)
-                q.push(temp->right);
-            else {
-                temp->right = newnode;
-            }
-        }
-    }
+    cout<<"Enter data to the left of "<<d;
+    root->left = buildTree(root->left);
+    cout<<"Enter data to the right of "<<d;
+    root->right = buildTree(root->right);
+    return root;
 }
 
-void BT::swap(Node *node)
+void nodeSwap(Node *root)
 {
-    if(node)
+    if(root)
     {
-        if(node->left || node->right)
+        if(root->left || root->right)
         {
-            Node *temp = node->left;
-            node->left = node->right;
-            node->right = temp;
+            Node *temp = root->left;
+            root->left = root->right;
+            root->right = temp;
         }
-        swap(node->right);
-        swap(node->left);
+        nodeSwap(root->right);
+        nodeSwap(root->left);
     }
 }
 
-int BT::height(Node* node)
+void rpostorder(Node *root)
 {
-    if(node == NULL) return 0;
-
-    int h1 = height(node->left);
-    int h2 = height(node->right);
-
-    return max(h1,h2)+1;
-}
-
-int BT::dia(Node* node)
-{
-    if(node == NULL) return 0;
-
-    int l = height(node->left);
-    int r = height(node->right);
-
-    int op1 = dia(node->left);
-    int op2 = dia(node->right);
-    int op3 = l+r+1;
-
-    return max(op1,max(op2,op3));
-}
-
-void BT::rpostorder(Node *node)
-{
-    if(node == NULL)
+    if(root == NULL)
         return;
 
-    rinorder(node->left);
-    rinorder(node->right);
-    cout<<node->data<<" ";
+    rpostorder(root->left);
+    rpostorder(root->right);
+    cout<<root->data<<" ";
 }
 
-void BT::rpreorder(Node *node)
+void rpreorder(Node *root)
 {
-    if(node == NULL)
+    if(root == NULL)
         return;
 
-    cout<<node->data<<" ";
-    rinorder(node->left);
-    rinorder(node->right);
+    cout<<root->data<<" ";
+    rpreorder(root->left);
+    rpreorder(root->right);
 }
 
-void BT::rinorder(Node *node)
+void rinorder(Node *root)
 {
-    if(node == NULL)
+    if(root == NULL)
         return;
 
-    rinorder(node->left);
-    cout<<node->data<<" ";
-    rinorder(node->right);
+    rinorder(root->left);
+    cout<<root->data<<" ";
+    rinorder(root->right);
 }
-  
-void BT::iinorder(Node *node)
+
+void iinorder(Node *root)
 {
     stack<Node*> s;
-    Node *curr = node;
+    Node *curr = root;
     while(!s.empty() or curr!=NULL)
     {
         if(curr!=NULL)
-        {    
+        {
             s.push(curr);
             curr = curr->left;
         }
-        
+
         curr = s.top();
         s.pop();
         cout<<curr->data<<" ";
@@ -183,11 +123,11 @@ void BT::iinorder(Node *node)
     }
 }
 
-void BT::ipreorder(Node *node)
+void ipreorder(Node *root)
 {
     stack<Node*> s;
-    Node *curr = node;
-    s.push(node);
+    Node *curr = root;
+    s.push(root);
     while(!s.empty() or curr!=NULL)
     {
         Node *t = s.top();
@@ -200,10 +140,10 @@ void BT::ipreorder(Node *node)
     }
 }
 
-void BT::ipostorder(Node *node)
+void ipostorder(Node *root)
 {
     stack<Node*> s1, s2;
-    s1.push(node);
+    s1.push(root);
     while(!s1.empty())
     {
         Node *t = s1.top();
@@ -215,9 +155,161 @@ void BT::ipostorder(Node *node)
             s1.push(t->right);
     }
     while(!s2.empty())
-    {   
+    {
         cout<<s2.top()->data<<" ";
         s2.pop();
     }
 }
 
+int height(Node* root)
+{
+    if(root == NULL) return 0;
+
+    int h1 = height(root->left);
+    int h2 = height(root->right);
+
+    return max(h1,h2)+1;
+}
+
+int dia(Node* root)
+{
+    if(root == NULL) return 0;
+
+    int l = height(root->left);
+    int r = height(root->right);
+
+    int op1 = dia(root->left);
+    int op2 = dia(root->right);
+    int op3 = l+r+1;
+
+    return max(op1,max(op2,op3));
+}
+
+int getLeafCount(Node *root)
+{
+    if(root == NULL)
+        return 0;
+    else if(root->left == NULL and root->right == NULL)
+        return 1;
+    else
+        return getLeafCount(root->right) + getLeafCount(root->left);
+}
+
+int getInternalNodes(Node *root)
+{
+    if(root == NULL or (root->right == NULL and root->left == NULL))
+        return 0;
+    else return 1 + getInternalNodes(root->left) + getInternalNodes(root->right);
+}
+
+void deleteTree(Node* root)
+{
+    if (root == NULL) return;
+
+    deleteTree(root->left);
+    deleteTree(root->right);
+
+    cout << "\n Deleting node: " << root->data;
+    delete root;
+}
+
+int main()
+{
+    Node *root = NULL;
+    buildTree(root);
+    int flag = 1, choice1;
+    while(flag)
+    {
+        int choice;
+        cout<<"======================================="<<endl;
+        cout<<"||                                   ||"<<endl;
+        cout<<"||       ~~~~~ BST MENU ~~~~~        ||"<<endl;
+        cout<<"||                                   ||"<<endl;
+        cout<<"||       1. Display BST              ||"<<endl;
+        cout<<"||       2. Swap all nodes           ||"<<endl;
+        cout<<"||       3. Height of tree           ||"<<endl;
+        cout<<"||       4. Number of leaf nodes     ||"<<endl;
+        cout<<"||       5. Number of internal nodes ||"<<endl;
+        cout<<"||       6. Delete all nodes         ||"<<endl;
+        cout<<"||                                   ||"<<endl;
+        cout<<"======================================="<<endl;
+        cout<<"\nEnter your choice: ";
+        cin>>choice;
+        
+        switch(choice)
+        {
+            case 1:
+                cout<<"\t1. Recursive Inorder\n\t2. Iterative Inorder\n\t3. Recursive Preorder\n\t4. Iterative Preorder\n\t5. Recursive Postorder\n\t6. Iterative Postorder\n\tEnter your choice: ";
+                cin>>choice1;
+                switch(choice1)
+                {
+                    case 1:
+                        rinorder(root);
+                        break;
+                        
+                    case 2:
+                        iinorder(root);
+                        break;
+                        
+                    case 3:
+                        rpreorder(root);
+                        break;
+                        
+                    case 4:
+                        ipreorder(root);
+                        break;
+                        
+                    case 5:
+                        rpostorder(root);
+                        break;
+                        
+                    case 6:
+                        ipostorder(root);
+                        break;
+                        
+                    default:
+                        cout<<"Wrong Input!"<<endl;
+                }
+                break;
+                
+            case 2:
+                nodeSwap(root);
+                cout<<"Nodes swapped!, Inorder Display: "<<endl;
+                iinorder(root);
+                break;
+                
+            case 3:
+                cout<<"Height of tree: "<<dia(root);
+                break;
+                
+            case 4:
+                cout<<"Number of leaf nodes: "<<getLeafCount(root)<<endl;
+                break;
+                
+            case 5:
+                cout<<"Number of internal nodes: "<<getInternalNodes(root);
+                break;
+                
+            case 6:
+                deleteTree(root);
+                break;
+                
+            default:
+                cout<<"Wrong input!"<<endl;
+        }
+    
+        label:
+        cout<<"\nDo you want to perform another operation? (1/0): ";
+        cin>>flag;
+        if(flag == 1)
+            continue;
+        else if(flag == 0)
+            break;
+        else
+        {
+            cout<<"Wrong input, try again!";
+            goto label;
+        }
+    }
+    return 0;
+}
